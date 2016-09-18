29aaab7b7146b4547cb16bb4e8b6f7c690a0a962
xuyan <informlarry@163.com>
2016-09-18 15:09:32 +0800

    1.4  Creating an Administration Site for Your Models

--- microblog/blog/admin.py
+++ microblog/blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'published_time', 'status')
+    list_filter = ('status', 'created_time', 'published_time', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = { 'slug': ('title',) }
+    raw_id_fields = ('author',)
+    date_hierarchy = 'published_time'
+    ordering = ['status', 'published_time']
+
+admin.site.register(Post, PostAdmin)
